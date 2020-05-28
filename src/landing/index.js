2098f30caf37807f20b17d8a769a60ee3c0cb433
undefined7887 <undefined7887@gmail.com>
2020-05-28 22:21:25 +0300

    Fixed typo in landing's index.js file

--- src/landing/index.js
+++ src/landing/index.js
@@ -1,11 +1,11 @@
 import Vue from 'vue'
-import Page from './components/App.vue'
+import App from './components/App.vue'
 
 Vue.config.productionTip = false;
 
 let app = new Vue({
   el: ".app",
   render: function (f) {
-    return f(Page)
+    return f(App)
   },
 });
